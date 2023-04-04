1ce731efe8ca7edf001d8fdade33fe7d6ae38321
sujipark <charmmming7@gmail.com>
2023-04-04 14:51:10 +0900

    3. 플러그인 과제코드 추가

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
-import "./main.css";
-
 import MainController from "./controllers/MainController.js";
+import "./main.css";
 
 document.addEventListener("DOMContentLoaded", () => {
   new MainController();
