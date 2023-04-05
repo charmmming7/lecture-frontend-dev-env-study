f6d9c3c5d1023de86cf1c5f5a81cdd9db8915d2c
sujipark <charmmming7@gmail.com>
2023-04-05 17:31:12 +0900

    3. 플러그인 과제 완성

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
-import MainController from "./controllers/MainController.js";
+// import MainController from "./controllers/MainController.js";
 import "./main.css";
 
-document.addEventListener("DOMContentLoaded", () => {
-  new MainController();
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//   new MainController();
+// });
+
+
+console.log(process.env.NODE_ENV);
+console.log(TWO);
+console.log(TWO2);
+console.log(String);
+console.log(api.domain);
